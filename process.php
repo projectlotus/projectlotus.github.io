90f29a0befb8a00407ebf0911320a9bac1724c7f
Biscuit <rjfarruggia@gmail.com>
2018-07-30 09:39:15 -0800

    store-frmwrk

--- process.php
+++ process.php
@@ -1,7 +1,7 @@
 <?php
 // Configure your Subject Prefix and Recipient here
 $subjectPrefix = '[Contact via website]';
-$emailTo       = '<YOUR_EMAIL_HERE>';
+$emailTo       = '<ENTER YOUR EMAIL HERE>';
 $errors = array(); // array to hold validation errors
 $data   = array(); // array to pass back data
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
